e44797b44832a596d28b980e7a7cfc5fab955b96
tiangj <635636771@qq.com>
2019-01-15 18:05:27 +0800

    订单列表代码

--- src/main/java/com/example/wwq/controller/WwqPostWayController.java
+++ src/main/java/com/example/wwq/controller/WwqPostWayController.java
@@ -1,9 +1,17 @@
 package com.example.wwq.controller;
 
 
+import com.example.wwq.entity.WwqPostWay;
+import com.example.wwq.service.IWwqPostWayService;
+import org.apache.commons.lang.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Map;
 
 /**
  * <p>
@@ -16,6 +24,33 @@ import org.springframework.stereotype.Controller;
 @Controller
 @RequestMapping("/wwqPostWay")
 public class WwqPostWayController {
+
+    @Autowired
+    private IWwqPostWayService wwqPostWayService;
+
+    @RequestMapping("list")
+    public String list(){
+      return "postWay/list";
+    }
+
+    @ResponseBody
+    @RequestMapping("listData")
+    public Map<String,Object> listData(Integer page, Integer limit,String name){
+        return null;
+    }
+
+
+    @RequestMapping("toAdd")
+    public String toAdd(Model model, String id){
+        WwqPostWay postWay=null;
+        if(StringUtils.isBlank(id)){
+            postWay=new WwqPostWay();
+        }else{
+            postWay=wwqPostWayService.selectById(id);
+        }
+        model.addAttribute("postWay",postWay);
+        return "postWay/add";
+    }
 
 }
 
